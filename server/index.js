44ca8d05aa33240a196b1270bada37761f864ecd
vibitha <vibithavijayan6@gmail.com>
2026-01-15 12:44:38 +0530

    frontend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 
-const allowedOrigins = ['http://localhost:5173/']
+const allowedOrigins = ['http://localhost:5173/','https://my-to-do-app-frontend-kappa.vercel.app/']
 
 app.use(express.json())
 app.use(cors({origin : allowedOrigins , credentials: true}))
