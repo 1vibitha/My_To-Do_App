1e4591ab9074fab22cfe52f92bac611c5452aa1f
vibitha <vibithavijayan6@gmail.com>
2026-01-15 13:57:14 +0530

    latest

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import todoRoutes from './routes/todoRoutes.js'
 const app = express()
 
 const allowedOrigins = [
-  'http://localhost:5173' 
+  'http://localhost:5173' ,'https://my-to-do-app-frontend-kappa.vercel.app'
 ]
 app.get('/', (req, res) => {
   res.send('API is running');
