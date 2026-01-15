c815fc410f64a7d010d6cec1df0011a2978bb3ec
vibitha <vibithavijayan6@gmail.com>
2026-01-15 12:42:29 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -7,9 +7,10 @@ import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 
+const allowedOrigins = ['http://localhost:5173/']
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin : allowedOrigins , credentials: true}))
 
 mongoose.connect(process.env.MONGODB_URL)
 .then(() => console.log("Database connected"))
